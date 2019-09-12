a6f758e199afba5b8bc44d887d051ec603082123
Rafael Grube <rafael.grube@solverit.com.br>
2019-09-12 15:02:04 -0300

    Ajuste nos divisores do breadcrumb

--- resources/js/components/Breadcrumb.vue
+++ resources/js/components/Breadcrumb.vue
@@ -3,10 +3,12 @@
     <div class="px-4 py-2 flex items-center bg-gray-200 text-gray-700">
       <a href="/home" class="text-blue-500">Home</a>
 
-      <div v-for="(nav, index) in items" :key="`breacrumb${index}`">
+      <div v-for="(nav, index) in items" :key="`breacrumb${index}`" class="flex items-center">
+        <div class="text-gray-600 mx-2">/</div>
         <a v-if="nav.uri" :href="nav.uri" class="breacrumb-item text-blue-500">{{ nav.title }}</a>
       </div>
 
+      <div class="text-gray-600 mx-2">/</div>
       <span class="breacrumb-item text-gray-600">{{ active }}</span>
     </div>
   </div>
@@ -17,11 +19,3 @@ export default {
   props: ["active", "items"]
 };
 </script>
-
-<style scoped>
-.breacrumb-item::before {
-  content: "/";
-  margin: 0 8px;
-  color: #666;
-}
-</style>
